c19b3f222fa313f690256e3e510922a1b6269780
Tara Meyerink <tarajmeyerink@gmail.com>
2017-10-23 20:36:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -62,7 +62,7 @@ end
 
 def turn(board)
  puts "Please enter 1-9:"
- user_input=gets.strip
+ user_input = gets.strip
  index = input_to_index(user_input)
  player_token = current_player(board)
  if valid_move?(board,index) == true
