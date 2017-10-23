c9f8694e15c88cfca60f558e0b732f631a1d3cd0
Tara Meyerink <tarajmeyerink@gmail.com>
2017-10-23 21:00:20 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -17,8 +17,7 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  index = user_input.to_i - 1
-  return index
+  user_input.to_i - 1  
 end
 
 def move(board, index, current_player)
